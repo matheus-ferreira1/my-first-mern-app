6c97425f0e056f54076632297f0f6dab9011612a
Matheus Ferreira <matheus.fer33@gmail.com>
2022-10-18 23:39:12 -0300

    create user on frontend implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(null);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) =>
+      setListOfUsers(response.data)
+    );
+  }, []);
+
+  const handleCreateUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      alert("User created successfully");
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="usersDisplay">
+        <div>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Age..."
+            onChange={(event) => setAge(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <button onClick={handleCreateUser}>Create User</button>
+        </div>
+        {listOfUsers.map((user) => (
+          <div style={{ border: "2px solid red" }} key={user._id}>
+            <h1>Name: {user.name}</h1>
+            <h3>Age: {user.age}</h3>
+            <h3>Username: {user.username}</h3>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
